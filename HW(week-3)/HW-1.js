afff44088e6b7bcab1d2c9508ffc0081c5fc8ab6
kajal shukla <shuklakajal1703@gmail.com>
2025-04-10 14:09:25 +0530

    Update HW-1.js

--- HW(week-3)/HW-1.js
+++ HW(week-3)/HW-1.js
@@ -1,5 +1,10 @@
-let N = 5;
-
-for (let i = 1; i <= N; i++){
-    console.log(i);
-}
+function celsiusToFahrenheit(celsius) {
+   
+    let fahrenheit = Math.floor((celsius * 9/5) + 32);
+    return fahrenheit;
+  }
+  
+  
+  console.log(celsiusToFahrenheit(0));   
+  console.log(celsiusToFahrenheit(100)); 
+  
