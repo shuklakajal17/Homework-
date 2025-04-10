c3ef5a23b8fe9c0ffa38c31c8a29a7e53e8d2e61
kajal shukla <shuklakajal1703@gmail.com>
2025-04-10 14:16:40 +0530

    Add files via upload

---
+++ HW(week-3)/HW-23.js
@@ -0,0 +1,10 @@
+function celsiusToFahrenheit(celsius) {
+   
+    let fahrenheit = Math.floor((celsius * 9/5) + 32);
+    return fahrenheit;
+  }
+  
+  
+  console.log(celsiusToFahrenheit(0));   
+  console.log(celsiusToFahrenheit(100)); 
+  
