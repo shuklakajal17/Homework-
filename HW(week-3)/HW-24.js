bab651810f0eb3a5948bfbd1c29b14734225c7d8
kajal shukla <shuklakajal1703@gmail.com>
2025-04-10 14:21:24 +0530

    Add files via upload

---
+++ HW(week-3)/HW-24.js
@@ -0,0 +1,6 @@
+function checkNumber(number) {
+    return number % 2 === 0 ? "even" : "odd";
+}
+
+console.log(checkNumber(4));
+console.log(checkNumber(9));
---
+++ HW(week-3)/HW-25.js
@@ -0,0 +1,7 @@
+function myNumber(N) {
+    for (let i = 1; i <= N; i++){
+        console.log(i * i);
+    }
+}
+myNumber(4);
+myNumber(3);
