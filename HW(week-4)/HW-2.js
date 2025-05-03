dbf4471e24139c4ea14445aba51532c8821a90e6
kajal shukla <shuklakajal1703@gmail.com>
2025-05-03 13:58:04 +0530

    Add files via upload

--- HW(week-4)/HW-2.js
+++ HW(week-4)/HW-2.js
@@ -1,13 +1,13 @@
-function countCharacter(str, variable) {
+function countCharacter( str1,str2) {
     let count = 0;
 
-    for (let i = 0; i < str.length; i++) {
-
-        if (str[i] === variable) {
+    for (let i = 0; i < str1.length; i++) {
+        if (str1[i] === str2) {
             count++;
         }
     }
     return count;
 }
-console.log(countCharacter("hello" , "l"));
-console.log(countCharacter("banana" , "a"));
+
+console.log(countCharacter("hello", "l"));
+console.log(countCharacter("banana", "a"));
