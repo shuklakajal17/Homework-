341628b68e003de6f28a42059eb6f4923412ad9e
kajal shukla <shuklakajal1703@gmail.com>
2025-06-10 23:27:08 +0530

    Add files via upload

---
+++ HW(week-5)/HW-19.js
@@ -0,0 +1,18 @@
+// const students = [
+//     { name: "john", age: 20, grade: "A"},
+//     { name: "Priya", age: 18, grade: "B"}
+// ];
+
+// for (let i = 0; i < students.length; i++) {
+//     const student = student[i];
+//     console.log(student.name, student.age, student.grade);
+// }
+
+const students = [
+    { name: "john", age: 20, grade: "A"},
+     { name: "Priya", age: 18, grade: "B"}
+ ];
+
+ for(const student of students) {
+    console.log(student.name, student.age, student.grade);
+ }
